6e23cd75f9b6803918d8bd4f6104a53008e58b9d
Agustin Fitipaldi <agustin@fitipaldi.com>
2025-07-05 01:41:09 -0700

    Initial Commit

---
+++ napkinTests/napkinTests.swift
@@ -0,0 +1,16 @@
+//
+//  napkinTests.swift
+//  napkinTests
+//
+//  Created by Agustin Fitipaldi on 7/5/25.
+//
+
+import Testing
+
+struct napkinTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
